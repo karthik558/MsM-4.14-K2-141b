96465bd3f1bf6133e3d21d29f4958aeb4c888b93
K A R T H I K <karthik.lal558@gmail.com>
2021-02-12 13:53:55 +0000

    Revert "mmc: Explicitly enable mmc card clocks before programing crypto key"
    This reverts commit d8aacd278af71ff98465d62799919e454d4d8184.
    
    Signed-off-by: K A R T H I K <karthik.lal558@gmail.com>

--- drivers/mmc/host/cmdq_hci-crypto-qti.c
+++ drivers/mmc/host/cmdq_hci-crypto-qti.c
@@ -30,8 +30,6 @@
 #define MINIMUM_DUN_SIZE 512
 #define MAXIMUM_DUN_SIZE 65536
 
-static struct mmc_host *mmc_host;
-
 static struct cmdq_host_crypto_variant_ops cmdq_crypto_qti_variant_ops = {
 	.host_init_crypto = cmdq_crypto_qti_init_crypto,
 	.enable = cmdq_crypto_qti_enable,
@@ -123,14 +121,10 @@ static int cmdq_crypto_qti_keyslot_program(struct keyslot_manager *ksm,
 		return -EINVAL;
 	}
 
-	mmc_host_clk_hold(mmc_host);
-
 	err = crypto_qti_keyslot_program(host->crypto_vops->priv, key,
 					 slot, data_unit_mask, crypto_alg_id);
 	if (err)
 		pr_err("%s: failed with error %d\n", __func__, err);
-
-	mmc_host_clk_release(mmc_host);
 
 	return err;
 }
@@ -151,15 +145,9 @@ static int cmdq_crypto_qti_keyslot_evict(struct keyslot_manager *ksm,
 		return -EINVAL;
 	}
 
-	mmc_host_clk_hold(mmc_host);
-
 	err = crypto_qti_keyslot_evict(host->crypto_vops->priv, slot);
 	if (err)
 		pr_err("%s: failed with error %d\n", __func__, err);
-		mmc_host_clk_release(mmc_host);
-		return err;
-	}
-	mmc_host_clk_release(mmc_host);
 
 	pm_runtime_put_sync(&host->mmc->card->dev);
 	val = atomic_read(&keycache) & ~(1 << slot);
@@ -249,11 +237,6 @@ int cmdq_host_init_crypto_qti_spec(struct cmdq_host *host,
 	 * descriptor would be used to pass crypto specific informaton.
 	 */
 	host->caps |= CMDQ_TASK_DESC_SZ_128;
-	mmc_host = host->mmc;
-	if (!mmc_host) {
-		err = -ENODEV;
-		goto out;
-	}
 
 	return 0;
 out:
@@ -336,11 +319,6 @@ int cmdq_host_init_crypto_qti_spec(struct cmdq_host *host,
 	 * descriptor would be used to pass crypto specific informaton.
 	 */
 	host->caps |= CMDQ_TASK_DESC_SZ_128;
-	mmc_host = host->mmc;
-	if (!mmc_host) {
-		err = -ENODEV;
-		goto out;
-	}
 
 	return 0;
 
